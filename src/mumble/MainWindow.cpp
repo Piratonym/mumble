1bcaebfb1766cad769f3db7307c9aea9f1a697b5
Piratonym <piratonym@piratonym.cc>
2017-01-16 01:20:58 +0100

    Fix context menu checkmark for uncounted users

--- src/mumble/MainWindow.cpp
+++ src/mumble/MainWindow.cpp
@@ -1523,6 +1523,7 @@ void MainWindow::qmUser_aboutToShow() {
 		qaUserDeaf->setChecked(p->bDeaf);
 		qaUserPrioritySpeaker->setChecked(p->bPrioritySpeaker);
 		qaUserLocalMute->setChecked(p->bLocalMute);
+		qaUserLocalNoCount->setEnabled(p->bLocalNoCount);
 		qaUserLocalIgnore->setChecked(p->bLocalIgnore);
 	}
 	updateMenuPermissions();
